2150d9023308afc1f89cbe6350d5b4853306cc61
gnuyhaa <guddk18@naver.com>
2025-07-10 09:43:24 +0900

    풀이 수정;

--- DB/250709.sql
+++ DB/250709.sql
@@ -9,15 +9,14 @@ CREATE TABLE Students (
 
 CREATE TABLE Grades (
     StudentID INT,
-    Math VARCHAR(255),
-    English VARCHAR(255),
-    Science VARCHAR(255),
-    FOREIGN KEY (StudentID) REFERENCES Students(StudentID) 
+    Math INT,
+    English INT,
+    Science INT
 );
 
 -- 실습 테이블 데이터 입력
 INSERT Students (Name, Age, Address)
-VALUES('홍길동','30','인천'),('이연걸','60','서울'),('이몽룡','42','대전'),('성춘향','27','경기');
+VALUES('홍길동',30,'인천'),('이연걸',60,'서울'),('이몽룡',42,'대전'),('성춘향',27,'경기');
 
 INSERT Grades (StudentID, Math, English, Science)
-VALUES ('1','90','80','50'),('2','69','76','65'),('3','98','87','97'),('4','87','67','79');
+VALUES (1,90,80,50),(2,69,76,65),(3,98,87,97),(4,87,67,79);
