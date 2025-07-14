802ded9715c4df5f3092ecd380323c35c90f339b
gnuyhaa <guddk18@naver.com>
2025-07-14 09:53:35 +0900

    실습 내용 정리

--- DB/250709.sql
+++ DB/250709.sql
@@ -90,34 +90,3 @@ UPDATE tb_employee SET salary = 4500 WHERE id = 3;
 UPDATE tb_employee SET salary = 7000 WHERE id = 4;
 UPDATE tb_employee SET salary = 5200 WHERE id = 5;
 
--------
--- 250710 pratice
-ALTER TABLE tb_employee
-ADD COLUMN department_id INT,
-ADD CONSTRAINT fk_department
-FOREIGN KEY (department_id) REFERENCES tb_department(id);
-
-UPDATE tb_employee SET department_id = 1 WHERE id IN (1, 2);
-UPDATE tb_employee SET department_id = 2 WHERE id = 3;
-UPDATE tb_employee SET department_id = 4 WHERE id = 4;
-UPDATE tb_employee SET department_id = 3 WHERE id = 5;
-
-SELECT d.name AS department_name, AVG(e.salary) AS avg_salary
-FROM tb_employee e
-JOIN tb_department d ON e.department_id = d.id
-GROUP BY d.name;
-
-SELECT name, salary
-FROM tb_employee
-ORDER BY salary DESC
-LIMIT 3;
-
-SELECT name, start_date
-FROM tb_project
-ORDER BY start_date ASC
-LIMIT 2;
-
-SELECT *
-FROM tb_employee
-ORDER BY id
-LIMIT 2 OFFSET 2;  
